64fb0a605271d9634f5fbaf38a1b8d50d6bf52b0
cyese <quangtuelamnguyen2003@gmail.com>
2024-06-28 10:51:44 +0000

    #feature: Login, signup, call API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,70 @@
-import './App.css'
-import LoginFrom from './Components/LoginForm/LoginForm'
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from "@components/Home/Home";
+import LoginForm from "@components/LoginForm/LoginForm";
+import RegisterForm from "@components/RegisterForm/RegisterForm"; 
+import React , { useState , useEffect } from "react";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    index : true,
+    element: <HomePage />
+  },
+  {
+    path: "login",
+    element: <LoginForm />
+  },
+  {
+    path: "register",
+    element: <RegisterForm />
+  }
+])
+
+// const Header = () => {
+//   const [isAuthenticated, setisAuthenticated] = useState(false);
+
+//   return (
+//   <Navbar bg="light" expand="lg">
+//     <Navbar.Brand href="/">Demo DevOps</Navbar.Brand>
+//     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//     <Navbar.Collapse id="basic-navbar-nav">
+//         <Nav className="ml-auto">
+//             {!isAuthenticated ? (
+//                 <>
+//                     <Nav.Link as={Link} to="/login">
+//                         Sign In
+//                     </Nav.Link>
+//                     <Nav.Link as={Link} to="/register">
+//                         Sign Up
+//                     </Nav.Link>
+//                 </>
+//             ) : (
+//                 <>
+//                     <Nav.Link as={Link} to="/home">
+//                         Home
+//                     </Nav.Link>
+//                     <Button
+//                         variant="outline-danger"
+//                         onClick={handleLogout}
+//                     >
+//                         Log Out
+//                     </Button>
+//                 </>
+//             )}
+//         </Nav>
+//     </Navbar.Collapse>
+//   </Navbar>
+// )
+
+// }
+
+const App = () => {
   return (
-    <div>
-      <LoginFrom />
-    </div>
+  <div>
+    {/* <Header /> */}
+    <RouterProvider router={router}/>
+  </div>
   )
 }
 
-export default App
+export default App;
--- frontend/src/Components/LoginForm/LoginForm.jsx
+++ frontend/src/Components/LoginForm/LoginForm.jsx
@@ -1,28 +1,51 @@
 import React, { useState } from 'react'
-import './LoginForm.css'
 import { FaUserAlt, FaEye, FaEyeSlash } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
 
-const LoginFrom = () => {
-  const [password, setpassword] = useState('');
-  
+import './LoginForm.css'
+import { Link } from 'react-router-dom';
+import callAPI from '@utils/api';
+
+const LoginForm = () => {
+  const [password, setpassword] = useState("");
+  const [username, setusername] = useState("");
+
   const [showPassword, setshowPassword] = useState(false);
   
+  const [fail, setfail] = useState(false)
+
   const togglePassword = () => {
     setshowPassword(!showPassword)
+  }
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    await callAPI('/api/auth/signin', 'POST', {username: username, password: password})
+      .then(
+        res => {
+          if (res && res["code"] === 200 ) {
+            window.location = '/home';
+          } else {
+            setfail(true);
+          }
+        }
+      ).catch(
+        setfail(true)
+      )
   }
 
   return (
     <div className='wrapper'>
       <form action="">
-        <h1> Login </h1>
+        <h1> LOGIN </h1>
+        {fail ? <p className='warning'> Invalid Credential </p> : <></>}
         <div className='input-box'>
-          <input type='text' placeholder='Username'  required/>
+          <input type='text' placeholder='Username' onChange={(e) => setusername(e)} required/>
           <FaUserAlt />
         </div>
         <div className="input-box">
-          <input type={showPassword ? 'text' : 'password'} placeholder='Password' required /> 
+          <input type={showPassword ? 'text' : 'password'} placeholder='Password' onChange={(p)=> setpassword(p) } required /> 
           <div className='icon' onClick={togglePassword}>
             {showPassword ? <FaEyeSlash />: <FaEye/>}
           </div>
@@ -30,16 +53,16 @@ const LoginFrom = () => {
         </div>
         <div className='remember-forgot'>
           <label><input type='checkbox' /> Rememeber me</label>
-          <a href='#'> Forgot password </a>
+          <Link to="#"> Forgot password </Link> 
         </div>
-        <button type="submit"> Login </button>
+        <button type="submit" onClick={submitForm}> Login </button>
         <button type="button"> Login with Google  <FcGoogle /> </button>
         <div className='register-link'>
-          <p> Don't have an account? <a href='#'> Register </a> </p>
+          <p> Don't have an account? <Link to="/register"> Register </Link> </p>
         </div>
-      </form>
+      </form> 
     </div>
   )
 }
 
-export default LoginFrom
+export default LoginForm
